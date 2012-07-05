597dcbab290a17595d1974e5c7dbdc30e3780ce5
teahupoo <sean@rec-rhymes.co.uk>
2012-07-05 13:23:12 +0200

    Update master

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,3 @@
 Twitter.configure do |config|
-  config.consumer_key = 'mTm8hfdCD5QyYjIQBUkdeQ'
-  config.consumer_secret = 'EEATarCzmIojNt57KoDAo94W6uAPUI1jitxhDGcudw'
-  config.oauth_token = '620088988-zhaQffRdy0QKRPgj0dN08b3SSagIE0KAdUZgoTMG'
-  config.oauth_token_secret = ' S5szPsaAboANQHdrO90U1g6YjdQuicBsNErCZiltQ'
+
 end
